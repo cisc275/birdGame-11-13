4f5d4764bc834646d7a20b16b8d4ebcf8c67c38c
Charles Lambert <clambert@udel.edu>
2019-04-05 15:46:06 -0400

    Initial Commit with some methods

---
+++ src/Controller.java
@@ -0,0 +1,14 @@
+import java.awt.event.ActionEvent;
+
+public class Controller {
+    View view;
+    Model model;
+
+    public Controller(){}
+
+    public void start(){}
+
+    public void pauseListener(ActionEvent e){}
+
+    public void keyPressedListener(ActionEvent e){}
+}
---
+++ src/View.java
@@ -0,0 +1,11 @@
+public class View {
+    PlayableBird bird;
+
+    public View(){}
+
+    public void update(){}
+
+    public void createImage(){}
+
+    public void paint(){}
+}
