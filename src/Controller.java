504d2284bed4f5bc9963a3a3a0fd98bfa2045b89
Charles Lambert <clambert@udel.edu>
2019-04-08 13:20:56 -0400

    Implements ActionListener and KeyListener now instead

--- src/Controller.java
+++ src/Controller.java
@@ -1,7 +1,9 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
 
-public class Controller {
+public class Controller implements ActionListener, KeyListener {
     View view;
     Model model;
 
@@ -9,7 +11,23 @@ public class Controller {
 
     public void start(){}
 
-    public void pauseListener(ActionEvent e){}
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
-    public void keyPressedListener(KeyEvent e){}
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
